648cc14c9450c0ceb4f8fe51dec6479bdcf63b6d
Pragati P <pragatiparsewar@gmail.com>
2026-06-28 18:04:36 +0530

    Added testng dependecies

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,25 +1,14 @@
 package runners;
 
-import java.io.*;
-import org.junit.AfterClass;
-import org.junit.runner.RunWith;
-//import com.cucumber.listener.Reporter;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import managers.FileReaderManager;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(
-	features = "src/test/resources/functionalTests",
-	glue= {"stepDefinitions"},
-	plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"},
-	monochrome = true
-	)
- 
-
-public class TestRunner {
-	@AfterClass
-	public static void writeExtentReport() {
-//		Reporter.loadXMLConfig(new File(FileReaderManager.getInstance().getConfigReader().getReportConfigPath()));
-	}
+    features = "src/test/resources/functionalTests",   // ✅ correct path
+    glue = {"stepDefinitions"},                        // ✅ matches your package
+    plugin = {"pretty", "html:target/cucumber-reports.html",
+              "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+    monochrome = true
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
